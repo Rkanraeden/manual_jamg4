f276ca963f83481455ca3d18fb96ca71dfd19971
Rkanraeden <lecortezc@gmail.com>
2022-06-20 16:42:34 -0400

    gem added, models and wiews created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  # get 'registrations/new'
   get 'home/index'
-  resources :stories
+  # resources :stories
   # root "stories#index"
   root "home#index"
-  
+
+  resources :users
+
+  get 'sign_up', to: 'users#new'
+  post 'sign_up', to: 'users#create'
+
+
 end
